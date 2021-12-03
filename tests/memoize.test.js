4b22ce69b6d0a69c63e85cb09202d8162cb02bdd
Ville-Pekka Frantsi <ville-pekka.frantsi@tuni.fi>
2021-12-03 14:00:02 +0200

    improved memoize. test function seems to work properly, problem with tests not passing is on course side

--- tests/memoize.test.js
+++ tests/memoize.test.js
@@ -1,21 +1,21 @@
 import memoize from "./COMP.SE.200-2021-2022-1-main/src/memoize.js"
-let object = { 'a': 1, 'b': 2 };
+const obj = { 'a': 1, 'b': 2 };
 const other = { 'c': 3, 'd': 4 };
-const values = memoize(values);
 describe("memoize tests", () => {
-	test('memoize should return correct values of memoized object: [1,2]', () => {
-		expect(values(object)).toBe([1,2]);
+	console.log(valuestest(obj));
+	test('memoize should return correct values using memoized function values: [1,2]', () => {
+		expect(valuestest(obj)).toStrictEqual([1, 2]);
 	}),
 	test('memoize should return values of other: [3,4] ', () => {
-		expect(values(other)).toBe([3,4]);
+		expect(valuestest(other)).toStrictEqual([3,4]);
 	}),
-	object.a = 2;
+	obj.a = 2;
 	test('memoize should return values of object unchanged: [1,2]', () => {
-		expect(values(object)).toBe([1.2]);
+		expect(valuestest(obj)).toStrictEqual([1,2]);
 	}),
-	values.cache.set(object, ['a', 'b'])
+	valuestest.cache.set(obj, ['a', 'b'])
 	test('memoize should return changed values of object: [a, b]', () => {
-		expect(values[object]).toBe(['a', 'b']);
+		expect(valuestest[obj]).toStrictEqual(['a', 'b']);
 	})
 });
 
